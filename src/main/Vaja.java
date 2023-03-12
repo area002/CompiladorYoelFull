b1a8faf076a840a9db28eb347bbfe469e8b81e87
area002 <yoelnm@outlook.com>
2023-03-12 17:19:16 -0400

    Compilador terminado

---
+++ src/main/Vaja.java
@@ -0,0 +1,16 @@
+package main;
+
+import Grafico.Ventana;
+
+public class Vaja {
+
+	public static void main(String[] args) {
+		new Ventana();
+	}
+	
+	
+	//tipos de datos aceptados: int y Boolean
+}
+
+
+
